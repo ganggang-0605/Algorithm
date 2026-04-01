c9dcff768021ef53a6cc24174afc22a67ea7d246
ganggang-0605 <rm478xbzmw@privaterelay.appleid.com>
2026-04-01 22:11:47 +0900

    [Silver IV] Title: 제로, Time: 64 ms, Memory: 33192 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+k = int(input())
+lst = []
+for _ in range(k):
+    n = int(input())
+    if n == 0:
+        lst.pop()
+    else:
+        lst.append(n)
+    
+print(sum(lst))
