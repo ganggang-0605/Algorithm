89709eb20b5eb18eb3ce0f03e3e434c68aa95291
ganggang-0605 <rm478xbzmw@privaterelay.appleid.com>
2026-04-27 14:44:31 +0900

    [Platinum III] Title: 열혈강호 3, Time: 10208 ms, Memory: 42376 KB -BaekjoonHub

---
+++ 백준/Platinum/11377. 열혈강호 3/열혈강호 3.py
@@ -0,0 +1,39 @@
+import sys
+sys.setrecursionlimit(2000)
+input = sys.stdin.readline
+
+n, m, k = map(int, input().split())
+adj = [[] for _ in range(n + 1)]
+for i in range(1, n + 1):
+    data = list(map(int, input().split()))
+    adj[i] = data[1:]
+
+match = [0] * (m + 1)
+visited = [0] * (m + 1)
+step = 0
+
+def dfs(x):
+    for nxt in adj[x]:
+        if visited[nxt] == step: 
+            continue
+        visited[nxt] = step
+        if match[nxt] == 0 or dfs(match[nxt]):
+            match[nxt] = x
+            return True
+    return False
+
+ans = 0
+for i in range(1, n + 1):
+    step += 1  
+    if dfs(i):
+        ans += 1
+
+extra = 0
+for i in range(1, n + 1):
+    step += 1
+    if dfs(i):
+        extra += 1
+        if extra == k:
+            break
+
+print(ans + extra)
