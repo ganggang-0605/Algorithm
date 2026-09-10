de1b89a60c0e2cca5bfa15f2ec81abf9db78b68d
ganggang-0605 <rm478xbzmw@privaterelay.appleid.com>
2026-09-10 15:02:27 +0900

    [level 1] Title: 최소직사각형, Time: 2.74 ms, Memory: 12.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,13 @@
+def solution(sizes):
+    for i in range(len(sizes)):
+        if sizes[i][0] > sizes[i][1]:
+            sizes[i][0], sizes[i][1] = sizes[i][1], sizes[i][0]
+            
+    sizes.sort(key= lambda x: x[0])
+    
+    x, y = 0, 0
+    for i in range(len(sizes)):
+        if sizes[i][0] > x: x = sizes[i][0]
+        if sizes[i][1] > y: y = sizes[i][1]
+        
+    return x * y
